e4069372827b496cca4ee8cf5b07dec05e4034cd
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2023-05-10 13:04:57 +0900

    schema 외래키 수정

--- backend/schema.sql
+++ backend/schema.sql
@@ -22,15 +22,34 @@ CREATE TABLE study
     joiningP INT,
     totalP INT,
     PRIMARY KEY(studyId),
-    FOREIGN KEY(writer) REFERENCE member.memId
+    FOREIGN KEY(writer) REFERENCE member(memId)
 )
 
 CREATE TABLE boot
 (
-
+    bootId INT,
+    title VARCHAR(50),
+    writer VARCHAR(10),
+    curDate DATE,
+    content VARCHAR(500),
+    category VARCHAR(10),
+    views INT,
+    likes INT,
+    PRIMARY KEY(bootId),
+    FOREIGN KEY(writer) REFERENCE member(memId)
 )
 
 CREATE TABLE QNA
 (
-    
+    QNAId INT,
+    title VARCHAR(50),
+    writer VARCHAR(10),
+    curDate DATE,
+    content VARCHAR(500),
+    category VARCHAR(10),
+    views INT,
+    likes INT,
+    reply VARCHAR(200),
+    PRIMARY KEY(QNAId),
+    FOREIGN KEY(writer) REFERENCE member(memId)
 )
