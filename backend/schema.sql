8e56e0bf09dafbb0387213fed765ace51bb40144
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2023-05-10 09:48:05 +0900

    커뮤니티 게시판 추가

--- backend/schema.sql
+++ backend/schema.sql
@@ -9,3 +9,28 @@ CREATE TABLE member
     feedback JSON,
     PRIMARY KEY(memId)
 );
+
+CREATE TABLE study
+(
+    studyId INT,
+    title VARCHAR(50),
+    writer VARCHAR(10),
+    curDate DATE,
+    content VARCHAR(500),
+    category VARCHAR(10),
+    views INT,
+    joiningP INT,
+    totalP INT,
+    PRIMARY KEY(studyId),
+    FOREIGN KEY(writer) REFERENCE member.memId
+)
+
+CREATE TABLE boot
+(
+
+)
+
+CREATE TABLE QNA
+(
+    
+)
