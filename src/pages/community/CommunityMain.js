85daa8cf25e334fe59a35b61dbcb167c28745d4d
parkheroine <juyen3409@gmail.com>
2023-05-21 17:35:11 +0900

    부트캠프후기 페이지 삭제, 컨텐츠 페이지 추가, 게시글 본문에 html 태그 파싱하는 코드 추가

--- src/pages/community/CommunityMain.js
+++ src/pages/community/CommunityMain.js
@@ -16,7 +16,7 @@ function CommunityMain() {
                     <hr style={{ width: '60%', margin: '0 auto' }} />
                     <Nav defaultActiveKey="/community" className="flex-column">
                         <Nav.Link href="/community/it"><div style={{ color: '#282c34' }}>IT 뉴스</div></Nav.Link>
-                        <Nav.Link href="/community/bootcamp"><div style={{ color: '#282c34' }}>부트캠프 후기</div></Nav.Link>
+                        <Nav.Link href="/community/content"><div style={{ color: '#282c34' }}>학습 컨텐츠</div></Nav.Link>
                         <Nav.Link href="/community/qna"><div style={{ color: '#282c34' }}>QnA</div></Nav.Link>
                     </Nav>
                 </div>
