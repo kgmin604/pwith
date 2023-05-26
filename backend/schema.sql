74f9b7a7244ab58d0dcc902a5fa1b2cfc88b6697
SCY <helenason@naver.com>
2023-05-26 19:55:14 +0900

    DB member 수정

--- backend/schema.sql
+++ backend/schema.sql
@@ -4,9 +4,8 @@ CREATE TABLE member
     memPw VARCHAR(20) NOT NULL,
     memName VARCHAR(10) NOT NULL,
     memEmail VARCHAR(20) NOT NULL,
-    isMento BOOLEAN,
+    isMento BOOLEAN DEFAULT 0,
     joinStudy JSON,
-    feedback JSON,
     PRIMARY KEY(memId)
 );
 
@@ -24,20 +23,6 @@ CREATE TABLE study
     PRIMARY KEY(studyId),
     FOREIGN KEY(writer) REFERENCES member(memId)
 )
-
--- CREATE TABLE boot
--- (
---     bootId INT,
---     title VARCHAR(50),
---     writer VARCHAR(10),
---     curDate DATE,
---     content VARCHAR(500),
---     category VARCHAR(10),
---     views INT,
---     likes INT,
---     PRIMARY KEY(bootId),
---     FOREIGN KEY(writer) REFERENCES member(memId)
--- )
 
 CREATE TABLE QNA
 (
@@ -63,14 +48,7 @@ CREATE TABLE reply (
 
     -- 1. post 테이블로 합치고 type 열 추가
     -- like 추가하고, joinP와 totalP는 studyroom으로 보내면 null값 방지 가능(추후 study는 studyroom과의 join으로 접근)
-    category INT NOT NULL,
+    type INT NOT NULL,
     postNum INT NOT NULL,
-    FOREIGN KEY(category, postNum) REFERENCES post(type, postId) on delete cascade
-
-    -- 2. nullable foreign key 또는 null 대신 0 저장
-    category INT NOT NULL,
-    studyId INT, -- can be null
-    QNAId INT, -- can be null
-    FOREIGN KEY(studyId) REFERENCES study(studyId) on delete cascade
-    FOREIGN KEY(QNAId) REFERENCES QNA(QNAId) on delete cascade
+    FOREIGN KEY(type, postNum) REFERENCES post(type, postId) on delete cascade
 )
