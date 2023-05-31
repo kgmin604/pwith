a860baca53252833110beb914a1af237d347aef9
SCY <helenason@naver.com>
2023-05-31 22:29:23 +0900

    portfolio 작성 및 조회 - 테스트 전

--- backend/controller/mentor_mgmt.py
+++ backend/controller/mentor_mgmt.py
@@ -9,14 +9,26 @@ class Portfolio() :
         self.content = content
         # 끌어올리기 구현 시 ON/OFF or date 추가 - DB에도
     
+    def getWriter(self) :
+        return str(self.writer)
+        
+    def getSubject(self) :
+        return str(self.subject)
+
+    def getImage(self) :
+        return str(self.image) # link ?
+
+    def getContent(self) :
+        return str(self.content)
+
     @staticmethod
     def create(writer, subject, image, content) :
         mysql_db = conn_mysql()
         cursor_db = mysql_db.cursor()
         sql = f"INSERT INTO mento(mentoId, mentiList, subject, mentoPic, content) VALUES ('{writer}', '{subject}', '{image}', '{content}')" ## str
-        cursor_db.execute(sql)
-        mysql_db.commit()
-        return None
+        done = cursor_db.execute(sql) ### print
+        mysql_db.commit() ### print
+        return done ## or, commit result (?)
 
     @staticmethod
     def loadAll() :
@@ -27,6 +39,7 @@ class Portfolio() :
         cursor_db.execute(sql)
 
         allP = cursor_db.fetchall()
+        print(type(allP)) ###
         return allP
 
     @staticmethod
