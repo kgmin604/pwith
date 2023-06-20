a4bff12c39c716c904feb99f3fd4850660eff1bf
SCY <helenason@naver.com>
2023-06-20 18:43:48 +0900

    로그인 체크 코드 주석 처리

--- backend/view/login.py
+++ backend/view/login.py
@@ -4,26 +4,26 @@ from controller.member_mgmt import Member
 
 bp = Blueprint('login', __name__, url_prefix='')
 
-@bp.route('/', methods=['POST']) # 테스트 전
-def chkSession() :
-    if request.method == 'POST' :
-        memInfo = {
-            'id': None,
-            'name': None,
-            'email': None
-        }
-        chk = request.get_json()['chkSession']
+# @bp.route('/', methods=['POST']) # 테스트 전
+# def chkSession() :
+#     if request.method == 'POST' :
+#         memInfo = {
+#             'id': None,
+#             'name': None,
+#             'email': None
+#         }
+#         chk = request.get_json()['chkSession']
 
-        if chk == 1:
-            if current_user.is_anonymous :
-                print('익명')
-            else :
-                memInfo['id'] = current_user.getId()
-                memInfo['name'] = current_user.getName()
-                memInfo['email'] = current_user.getEmail()
-                print('전달 완료')
+#         if chk == 1:
+#             if current_user.is_anonymous :
+#                 print('익명')
+#             else :
+#                 memInfo['id'] = current_user.getId()
+#                 memInfo['name'] = current_user.getName()
+#                 memInfo['email'] = current_user.getEmail()
+#                 print('전달 완료')
 
-        return jsonify(memInfo)
+#         return jsonify(memInfo)
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login() :
