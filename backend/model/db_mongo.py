81e872b3ff3372e8db4a249d31b24e7d72db082c
SCY <helenason@naver.com>
2023-06-21 01:49:44 +0900

    mongoDB 공유 서버 연결

--- backend/model/db_mongo.py
+++ backend/model/db_mongo.py
@@ -1,20 +1,14 @@
 import pymongo
 
-MONGO_HOST = 'localhost'
+MONGO_SERVER = 'mongodb+srv://pwith:pwith1234@cluster0.ezfau5x.mongodb.net/'
 
-mongo_conn = pymongo.MongoClient('mongodb://%s' % MONGO_HOST)
+mongo_conn = pymongo.MongoClient(MONGO_SERVER)
 
 def conn_mongodb() :
     try:
         mongo_conn.admin.command('ismaster')
         pwith_db = mongo_conn.pwith_db
     except:
-        mongo_conn = pymongo.MongoClient('mongodb://%s' % MONGO_HOST)
+        mongo_conn = pymongo.MongoClient(MONGO_SERVER)
         pwith_db = mongo_conn.pwith_db
     return pwith_db
-
-
-# 컴퓨터에 MongoDB 없으면 설치해야 해.
-# MongoDB Compass로 pwith_db라는 데이터베이스 생성하고 ITnews_crawling, lecture_crawling, book_crawling이라는 collection 세 개 추가하기
-
-# 추가로, pymysql 모듈 설치했듯 pymongo 모듈도 설치해야 해. venv 가상환경에서 pip install pymongo 입력하기.
