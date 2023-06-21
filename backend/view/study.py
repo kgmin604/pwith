34d763a105bcfee692ad241d355b37ffbab00892
SCY <helenason@naver.com>
2023-06-21 22:25:46 +0900

    스터디글 main 페이지 수정

--- backend/view/study.py
+++ backend/view/study.py
@@ -19,23 +19,23 @@ def show():
 
         if (searchType is None) or (searchValue is None) : # 전체 글 출력
             result = []
+
             posts = studyPost.getStudy()
+
             for i in range(len(posts)):
                 post = {
                         'id' : posts[i][0],
-                        'type' : posts[i][1],
                         'title' : posts[i][2],
                         'writer' : posts[i][3],
-                        'content' : posts[i][4],
                         'curDate' : posts[i][5],
-                        'category' : posts[i][6],
                         'likes' : posts[i][7],
-                        'views' : posts[i][9],
-                        'liked' : posts[i][9]
+                        'views' : posts[i][9]
                     }
                 post['curDate'] = studyPost.getFormattedDate(posts[i][5])
+
                 result.append(post)
-            return result
+
+            return jsonify(result)
 
         else : # 글 검색
             posts = []
