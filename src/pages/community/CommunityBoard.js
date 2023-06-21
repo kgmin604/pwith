135a9b98729dc61c3c498260d5d01f2a3fb7f419
kgmin604 <kkm509917@gmail.com>
2023-06-21 16:14:55 +0900

    커뮤니티 메인 map 구현까지 백업

--- src/pages/community/CommunityBoard.js
+++ src/pages/community/CommunityBoard.js
@@ -7,35 +7,53 @@ import { Form, Nav, Stack, Button, Table } from "react-bootstrap";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function CommunityBoard(){
-    let navigate=useNavigate();
+    let navigate = useNavigate();
+
+    const dummy = [
+        {'title': '로딩중...', 'date':'0000-00-00', 'url':0},
+        {'title': '로딩중...', 'date':'0000-00-00', 'url':0},
+        {'title': '로딩중...', 'date':'0000-00-00', 'url':0}
+    ]
+    const dummy2 = [
+        {'title': '로딩중...', 'date':'0000-00-00', 'postId':1},
+        {'title': '로딩중...', 'date':'0000-00-00', 'postId':1},
+        {'title': '로딩중...', 'date':'0000-00-00', 'postId':1}
+    ]
+    const dummy3 = [
+        {'title': '로딩중...', 'date':'0000-00-00', 'url':0},
+        {'title': '로딩중...', 'date':'0000-00-00', 'url':0},
+        {'title': '로딩중...', 'date':'0000-00-00', 'url':0}
+    ]
+
+    let [itList, setItList] = useState(dummy);
+    let [qnaList, setQnaList] = useState(dummy2);
+    let [contentList, setContentList] = useState(dummy3);
 
     return(
         <div className="CommunityBoard">
             <Table bordered hover >
-                        <thead>
-                            <tr >
-                                <th colSpan={3} className="posting-header" >
-                                    <div className="tableHead">
-                                        <div >최신 IT 뉴스</div>
-                                        <span onClick={() => navigate("../it")}>(+)</span>
-                                    </div>
-                                </th>
+                <thead>
+                    <tr>
+                        <th colSpan={3} className="posting-header" >
+                            <div className="tableHead">
+                                <div>최신 IT 뉴스</div>
+                                <span onClick={() => navigate("../it")}>(+)</span>
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody className="it-list">
+                {
+                    itList.map((it,i)=>{
+                        return (
+                            <tr className="item" key={i}>
+                                <td>{it['title']}</td>
+                                <td>{it['date']}</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>글제목입니다</td>
-                                <td>날짜</td>
-                            </tr>
-                            <tr>
-                                <td>글제목입니다</td>
-                                <td>날짜</td>
-                            </tr>
-                            <tr>
-                                <td>글제목입니다</td>
-                                <td>날짜</td>
-                            </tr>
-                        </tbody>
+                        );
+                    })
+                }
+                </tbody>
                     </Table>
 
                     <Table bordered hover >
