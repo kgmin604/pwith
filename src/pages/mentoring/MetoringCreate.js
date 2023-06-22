972ff8592e48fee4e8e600128009d5060a12fe02
parkheroine <juyen3409@gmail.com>
2023-06-22 22:38:17 +0900

    멘토링 이미지 전송 수정-디비 저장 안되는 에러

--- src/pages/mentoring/MetoringCreate.js
+++ src/pages/mentoring/MetoringCreate.js
@@ -53,10 +53,6 @@ function MentoringCreate() {
             alert("요청을 처리하지 못했습니다.");
           });
       }
-      
-    useEffect(() => {
-        console.log(portfolio);
-    }, [portfolio]);
     
 
     const handleWordClick = (word) => {
