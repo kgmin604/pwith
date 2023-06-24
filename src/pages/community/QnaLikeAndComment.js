389498ed19ae586e14d9d92744ef967023d2a076
parkheroine <juyen3409@gmail.com>
2023-06-24 18:14:21 +0900

    Revert "스터디 댓글 수정 삭제 구현"
    This reverts commit 13d7163353c50008010eed3d0e30aabf8906f1ce.

--- src/pages/community/QnaLikeAndComment.js
+++ src/pages/community/QnaLikeAndComment.js
@@ -13,18 +13,14 @@ import heartFull from "../study/img/heart-full.png"
 import comment from "../study/img/comment.png"
 import moreImg from "../study/img/more.png"
 
-function LikeAndComment(props) {//좋아요 완료
+function LikeAndComment(props) {
+
     let user = useSelector((state) => state.user);
     const id = props.id;
-
     const [likes, setLikes] = useState(0);
-    const [liked,setLiked]=useState(0);
+    const [liked,setLiked]=useState(false);
     const [reply,setReply]=useState([]);
     const [replyNum, setReplyNum] = useState(0);
-
-    const [more, setMore] = useState(false);
-    const [moreId, setMoreId] = useState(-1);
-    const [updateId, setUpdateId] = useState(-1);
 
     useEffect(()=>{
         if(props!=undefined){
@@ -37,22 +33,23 @@ function LikeAndComment(props) {//좋아요 완료
         
     },[props])
 
+    const [more, setMore] = useState(false);
+
     const sendLikeSignal = () => {
-        axios.post(`/community/qna/${id}/like`, {
+        axios.post(`/study/${id}/like`, {
             postId: id
         })
             .then(function (response) {
-                axios.get(`/community/qna/${id}/like`)
+                axios.get(`/study/${id}/like`)
                     .then((response) => {
                         console.log(response.data)
-                        if(liked===1){//원래 좋아요 눌러져있음-다시 누르면 감소
+                        if(liked){
                             setLikes(likes-1);
-                            setLiked(0);
                         }
                         else{
                             setLikes(likes+1);
-                            setLiked(1);
                         }
+                        setLiked(!liked);
                         
                     })
                     .catch(function (error) {
@@ -63,12 +60,12 @@ function LikeAndComment(props) {//좋아요 완료
                 // 요청 실패 시 처리할 로직
             });
     };
-    function clickHeart(){
-        if(user.id===null){
-            alert("로그인이 필요합니다")
-            return;
-        }
-        sendLikeSignal();
+
+    function updateComment() {
+
+    }
+    function deleteComment() {
+
     }
 
     function createComment(content) {
@@ -81,8 +78,8 @@ function LikeAndComment(props) {//좋아요 완료
                 const newReply = [
                     ...reply,
                     {
-                        "content": `${content}`,
-                        "commentId": response.data.commentId,
+                        "comment": `${content}`,
+                        "commentId": response.data.replyId,
                         "date":`${response.data.date}`,
                         "writer":`${user.id}`
                     }
@@ -93,51 +90,6 @@ function LikeAndComment(props) {//좋아요 완료
             })
             .catch(function (error) {
                 // 오류발생시 실행
-            });
-    }
-
-    function updateComment(commentId, content) {
-        axios.put(`/community/qna/${id}`, {
-            commentId: `${commentId}`,
-            content: `${content}`
-        })
-            .then(function (response) {
-                console.log(response);
-                const updatedReply = reply.map(reply => {
-                    if (reply.commentId === commentId) {
-                        reply.content= `${content}`;
-                    }
-                    setUpdateInput("");
-                    return reply;
-                });
-                setReply(updatedReply);
-                console.log(reply);
-                alert("댓글 수정 성공");
-                setUpdateInput("");
-                setUpdateId(-1)
-            }).catch(function (error) {
-                // 오류발생시 실행
-            })
-    }
-
-    function deleteComment(commentId) {
-        axios.delete(`/community/qna/${id}`, {
-            data: {
-                commentId: `${commentId}`
-            }
-        })
-            .then(function (response) {
-                const filteredReply = reply.filter(reply => reply.commentId !== commentId);
-                setReply(filteredReply);
-                setReplyNum(replyNum - 1);
-                setMore(false);
-                alert("댓글 삭제 성공");
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
             });
     }
 
@@ -156,34 +108,17 @@ function LikeAndComment(props) {//좋아요 완료
             createComment(inputValue);
         }
     };
-    const [updateInput, setUpdateInput] = useState('');
-    const handleClick = (content, id) => {
-        setUpdateId(id);
-        setMore(false);
-        setUpdateInput(content);
-    }
-    const handleUpdateChange = (event) => {
-        setUpdateInput(event.target.value);
-    };
-    const handleUpdateSubmit = (event, commentId) => {
-        event.preventDefault();
-        console.log(updateInput);
-        updateComment(commentId, updateInput);
-    };
 
-    function clickMore(id) {
-        setMoreId(id);
-        setMore(!more);
-        console.log(moreId);
-    }
+
 
     return (
         <div className='likeAndComment'>
             <div className='align-row'>
                 <div className='align-row'>{/* 하트 */}
-                    {liked===0? <img src={heartOutline} className="heart" onClick={() => clickHeart()} />
-                        : <img src={heartFull} className="heart" onClick={() => clickHeart()} />
+                    {!liked? <img src={heartOutline} className="heart" onClick={() => sendLikeSignal()} />
+                        : <img src={heartFull} className="heart" onClick={() => sendLikeSignal()} />
                     }
+                    <img src={heartFull} className="heart"/>
                     <span style={{ width: '5px' }}></span>
                     {likes}
                 </div>
@@ -201,42 +136,47 @@ function LikeAndComment(props) {//좋아요 완료
                 const date=JSON.stringify(k.date).slice(3,11);
 
                 return (
-                    <div key={k.commentId}>{/* 댓글하나 */}
-                        <div className='align-row'>
-                            <img img src={comment} className='comment' />
-                            <span style={{ width: '5px' }}></span>
-                            <div className='align-side'>
-                                <div style={{ textAlign: 'start' }}>
-                                    <div style={{ fontSize: '13px' }}>{k.writer}</div>
-                                    {k.commentId === updateId ? <><Form onSubmit={(event) => handleUpdateSubmit(event, updateId)} className='align-side' tyle={{ width: '150%' }}>
+                    <div  key={k.commentId}>{/* 댓글하나 */}
+                    <div className='align-row'>
+                        <img img src={comment} className='comment' />
+                        <span style={{ width: '5px' }}></span>
+                        <div className='align-side'>
+                            <div style={{ textAlign: 'start' }}>
+                                <div style={{fontSize:'13px'}}>{k.writer}</div>
+                                {/* {updateInput ? <>
+                                    <Form onSubmit={handleUpdateSubmit(k.commentId)} style={{ width: '90%' }}>
                                         <Form.Control
                                             className="me-auto"
                                             value={updateInput}
                                             onChange={handleUpdateChange}
                                         />
+                                        <Button variant="blue" type="submit" style={{ width: '10%' }}>수정</Button>
                                     </Form>
-                                    </> : k.comment}
+                                </> : k.comment} */}
+                                {k.comment}
+                            </div>
+
+                            <div style={{ textAlign: 'end' }}>
+                                {user.id === k.menti ? <img src={moreImg} className="more" onClick={() => setMore(!more)} />
+                                    : <div style={{width:'30px',height:'30px'}}></div>}
+                                <div>
+                                    {more === true ? <ListGroup>
+                                        <ListGroup.Item action onClick={() =>{}}>수정</ListGroup.Item>
+                                        <ListGroup.Item action onClick={() => {}}>삭제</ListGroup.Item>
+                                    </ListGroup> : null}
                                 </div>
-                                <div style={{ textAlign: 'end' }}>
-                                    {user.id === k.writer ? <img src={moreImg} className="more" onClick={() => clickMore(k.commentId)} />
-                                        : null}
-                                    <div>
-                                        {(more == true) && (moreId === k.commentId) ? <ListGroup>
-                                            <ListGroup.Item action onClick={() => handleClick(k.reply, k.commentId)}>수정</ListGroup.Item>
-                                            <ListGroup.Item action onClick={() => deleteComment(k.commentId)}>삭제</ListGroup.Item>
-                                        </ListGroup> : null}
-                                    </div>
-                                </div>
+                                <div style={{ color: '#888888',fontSize:'12px' }}>{date}</div>
                             </div>
                         </div>
-                        <hr />
+                    </div>
+                    <hr/>
                     </div>
 
                 )
             })}
 
             <div className='align-side' style={{margin:'10px'}}>{/* 댓글달기*/}
-                <Form onSubmit={handleSubmit}  className='align-side' style={{ width: '100%' }}>
+                <Form onSubmit={handleSubmit}  className='align-side'>
                     <Form.Control style={{ width: '90%' }}
                         className="me-auto"
                         placeholder="저도 이게 궁금해요!"
