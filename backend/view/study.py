00aee98a5e1b774623e5ac8f6b9e1385c5a9cb7e
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2023-06-25 22:50:27 +0900

    최근 스터디 상세페이지 로딩 오류 해결, qna 날짜 형식 변경

--- backend/view/study.py
+++ backend/view/study.py
@@ -141,7 +141,15 @@ def showDetail(id) :
         
         roomId= studyPost.getRoomId(id) #roomName 조회위해서 미리 변수로 리턴받음
         
-        isApplied = f'"{current_user.getId()}"' in StudyRoom.getStudentList(roomId)
+        # isApplied = f'"{current_user.getId()}"' in StudyRoom.getStudentList(roomId)
+        studentList = StudyRoom.getStudentList(roomId)
+        if studentList is not None:
+            isApplied = f'"{current_user.getId()}"' in studentList
+        else:
+            isApplied = None
+    # 적절한 오류 처리 또는 기본값 설정을 수행하십시오.
+
+        
         # print("==== 스터디 신청이 되었는가 ====")
         # print(isApplied)
         # print("================")
