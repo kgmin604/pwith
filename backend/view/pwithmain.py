39620c7ddc98ce6ee3fda634700e0f19d7baa1c3
choi-jyoon <72444141+choi-jyoon@users.noreply.github.com>
2023-08-02 09:57:48 +0900

    refactor :  메인페이지 리턴 형식 리팩토링

--- backend/view/pwithmain.py
+++ backend/view/pwithmain.py
@@ -6,44 +6,48 @@ from backend.model.db_mongo import conn_mongodb
 
 main_bp = Blueprint('pwithmain', __name__, url_prefix='')
 
-@main_bp.route('/', methods = ['GET', 'POST'])
+@main_bp.route('/', methods = ['POST'])
 def showStudy():
-    if request.method == 'POST':
-         
-        chk = request.get_json()['chkSession']
-        
-        if chk == 0:
-            studyList = []
-            newsList = []
-            mentoringList = []
+    chk = request.get_json()['chkSession']
+    
+    if chk == 0:
+        studyList = []
+        newsList = []
+        mentoringList = []
 
-            posts = studyPost.getNStudy(5)
-            for post in posts :
-                post = {
-                    'id' : post[0],
-                    'title' : post[1],
-                }
-                studyList.append(post)
-            # print(studyList)
+        posts = studyPost.getNStudy(5)
+        for post in posts :
+            post = {
+                'id' : post[0],
+                'title' : post[1],
+            }
+            studyList.append(post)
+        # print(studyList)
 
-            news_db = conn_mongodb().ITnews_crawling.find().sort('_id', -1).limit(5)
-            for news in news_db :
-                newsList.append({
-                    'title' : news['title'],
-                    'url' : news['url']
-                })
-                
-            mentorings = Portfolio.getNmentoring()
-            for portfolio in mentorings:
-                portfolio = {
-                    'id' : portfolio[0],
-                    'brief' : portfolio[1]
-                }
-                mentoringList.append(portfolio)
-
-            return jsonify({
-                'study' : studyList,
-                'news' : newsList,
-                'mentoring' : mentoringList
+        news_db = conn_mongodb().ITnews_crawling.find().sort('_id', -1).limit(5)
+        for news in news_db :
+            newsList.append({
+                'title' : news['title'],
+                'url' : news['url']
             })
+            
+        mentorings = Portfolio.getNmentoring()
+        for portfolio in mentorings:
+            portfolio = {
+                'id' : portfolio[0],
+                'brief' : portfolio[1]
+            }
+            mentoringList.append(portfolio)
+
+        return {
+            'study' : studyList,
+            'news' : newsList,
+            'mentoring' : mentoringList
+        }
         
+@main_bp.route('/', methods = ['GET', 'POST'])
+def alarm():
+    # 알림창
+    return{
+        'data':None
+    }
