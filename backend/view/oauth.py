3315280d8323f36991cbf20a9603280d71b2d1b9
SCY <helenason@naver.com>
2023-08-19 03:26:08 +0900

    feat: 네이버 소셜로그인

--- backend/view/oauth.py
+++ backend/view/oauth.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, redirect
-from urllib.parse import urlencode
 import requests
 import json
 
@@ -8,7 +7,7 @@ from backend import config
 oauth_bp = Blueprint('oauth', __name__, url_prefix = '')
 
 @oauth_bp.route('/oauth/callback/google', methods = ['GET'])
-def google_callback(): # access token 받기
+def google_callback(): # access token
     
     code = request.args.get('code')
 
@@ -26,4 +25,49 @@ def google_callback(): # access token 받기
         grant_type = grant_type
     ))
 
+    # json.loads(resp.text).get_json
+    # token = json.loads(resp.text).get('access_token')
+
+    # login_user()
+
     return json.loads(resp.text) # str to json
+
+@oauth_bp.route('/oauth/callback/naver', methods = ['GET'])
+def naver_callback():
+    
+    code = request.args.get('code')
+
+    token_endpoint = config.NAVER_TOKEN_ENDPOINT
+    info_endpoint = config.NAVER_INFO_ENDPOINT
+    client_id = config.NAVER_CLIENT_ID
+    client_secret = config.NAVER_CLIENT_SECRET
+    redirect_uri = config.NAVER_REDIRECT_URI
+    grant_type = 'authorization_code'
+
+    token_response = requests.get(token_endpoint, params = dict(
+        code = code,
+        client_id = client_id,
+        client_secret = client_secret,
+        grant_type = grant_type
+    ))
+
+    access_token = token_response.json().get("access_token")
+
+    info_response = requests.get(
+        info_endpoint,
+        headers = {
+            'Authorization' : f'Bearer {access_token}'
+        }
+    )
+    info = info_response.json()
+
+    if info.get('resultcode') == '00' :
+        status = 200
+    else :
+        status = 401
+
+    return {
+        'status' : status,
+        'message' : info.get('message'),
+        'data' : info.get('response')
+    }
