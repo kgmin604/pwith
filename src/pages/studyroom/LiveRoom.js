40ba12be7dfa1f02536b0a78cbef762acead2c55
parkheroine <juyen3409@gmail.com>
2023-08-30 09:53:59 +0900

    fix: 스터디룸 채팅 입력창 추가

--- src/pages/studyroom/LiveRoom.js
+++ src/pages/studyroom/LiveRoom.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./liveroom.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons/faMicrophone";
 import { faMicrophoneSlash } from "@fortawesome/free-solid-svg-icons/faMicrophoneSlash";
@@ -9,6 +9,7 @@ import { faVideoSlash } from "@fortawesome/free-solid-svg-icons/faVideoSlash";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -22,10 +23,32 @@ const LiveRoom = () => {
     const [showPeople, setShowPeople] = useState(false)
     const [showChat, setShowChat] = useState(false)
 
+    const [isClicked, setIsClicked] = useState(false);
+    const handleDivClick = () => {
+        setIsClicked(true);
+    };
+
     const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setIsClicked(false);
+        }
+    };
+
+    const textRef = useRef();
+    const ref = useRef(null);
+    const handleResizeHeight = useCallback(() => {
+        textRef.current.style.height = textRef.current.scrollHeight + "px";
+    }, []);
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
-        <div className="live-room">
+        <div className="live-room" ref={ref}>
             <div className="left-space">
                 {showPeople && <div className="people-list">
                     {data.map((a, i) => {
@@ -41,18 +64,28 @@ const LiveRoom = () => {
             </div >
 
             {showChat && <div className="right-space">
-                <div className="header">채팅<FontAwesomeIcon icon={faXmark} onClick={() => { setShowChat(false) }} /></div>
-                <div className="chat-user"><div className="chat-name">박주연</div><div className="chat-time">오후 5:40</div></div>
-                <div className="chat-content">울랄라</div>
-                <div className="chat-content">어쩌구</div>
-                <div className="chat-content">움냔냥</div>
-                <div className="chat-user"><div className="chat-name">김경민</div><div className="chat-time">오후 5:40</div></div>
-                <div className="chat-content">코딩 공부 같이 해요</div>
-                <div className="chat-content">재밌는 코딩 공부</div>
-                <div className="chat-user"><div className="chat-name">박주연</div><div className="chat-time">오후 5:40</div></div>
-                <div className="chat-content">울랄라</div>
-                <div className="chat-content">어쩌구</div>
-                <div className="chat-content">움냔냥</div>
+                <div>
+                    <div className="header">채팅<FontAwesomeIcon icon={faXmark} onClick={() => { setShowChat(false) }} /></div>
+                    <div className="chat">
+                        <div className="chat-user"><div className="chat-name">박주연</div><div className="chat-time">오후 5:40</div></div>
+                        <div className="chat-content">울랄라</div>
+                        <div className="chat-content">어쩌구</div>
+                        <div className="chat-content">움냔냥</div>
+                        <div className="chat-user"><div className="chat-name">김경민</div><div className="chat-time">오후 5:40</div></div>
+                        <div className="chat-content">코딩 공부 같이 해요</div>
+                        <div className="chat-content">재밌는 코딩 공부</div>
+                        <div className="chat-user"><div className="chat-name">박주연</div><div className="chat-time">오후 5:40</div></div>
+                        <div className="chat-content">울랄라</div>
+                        <div className="chat-content">어쩌구</div>
+                        <div className="chat-content">움냔냥</div>
+                    </div>
+                </div>
+                <form className={`chat-input ${isClicked ? 'clicked' : ''}`} onClick={handleDivClick}>
+                    <textarea ref={textRef} onInput={handleResizeHeight} />
+                    <div className="iconWrapper">
+                        <FontAwesomeIcon icon={faPaperPlane} color={'white'} />
+                    </div>
+                </form>
             </div>}
 
 
@@ -84,4 +117,4 @@ const LiveRoom = () => {
     )
 }
 
-export default LiveRoom
+export default LiveRoom;
