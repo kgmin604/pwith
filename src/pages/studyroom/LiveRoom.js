4028a32e74d98611e4574596bfdead8d21285c30
parkheroine <juyen3409@gmail.com>
2023-09-10 03:01:30 +0900

    feat: 카메라, 오디오

--- src/pages/studyroom/LiveRoom.js
+++ src/pages/studyroom/LiveRoom.js
@@ -23,9 +23,11 @@ const LiveRoom = () => {
     const [isMikeOn, setIsMikeOn] = useState(true)
     const [isCameraOn, setIsCameraOn] = useState(true)
     const [isCodeOn, setIsCodeOn] = useState(true)
-    const audioRef = useRef(null);
-    let myStream
-    const [showPeople, setShowPeople] = useState(false)
+    const [videoStream, setVideoStream] = useState(null); // 비디오 스트림을 따로 저장
+
+    const videoRef = useRef(null);
+    const [myStream, setMyStream] = useState(null)
+    const [showPeople, setShowPeople] = useState(true)
     const [showChat, setShowChat] = useState(false)
 
     const [isClicked, setIsClicked] = useState(false);
@@ -44,18 +46,19 @@ const LiveRoom = () => {
     };
 
 
-    async function getMedia(callback) {
+    async function getMedia() {
         const initialConstrains = {
-            'video': false,
+            'video': true,
             'audio': true,
         };
         try {
-            myStream = await navigator.mediaDevices.getUserMedia(initialConstrains);
+            const newStream = await navigator.mediaDevices.getUserMedia(initialConstrains);
+            videoRef.current.srcObject = newStream
+            setMyStream(newStream)
         } catch (e) {
             console.log(e);
         }
     }
-
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
         getMedia()
@@ -64,26 +67,22 @@ const LiveRoom = () => {
         };
     }, []);
 
+
     const onClickMike = async () => {
-        if (audioRef.current) {
-            const audio = audioRef.current;
-            if (isMikeOn) {
-                audio.srcObject = null;
-            } else {
-                try {
-                    // 마이크 켜기
-                    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-                    audio.srcObject = stream;
-                } catch (error) {
-                    console.error("Failed to get audio stream: ", error);
-                }
-            }
-            setIsMikeOn(!isMikeOn);
-        }
+        myStream
+            .getAudioTracks()
+            .forEach((track) => (track.enabled = !track.enabled));
+        setIsMikeOn((prev) => !prev)
     };
 
+    const onClickCamera = async () => {
+        setIsCameraOn((prev) => !prev);
+        myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    };
+
+
     const onClickContentStream = () => {
-        setIsCameraOn(!isCameraOn)
+        //컨텐츠
     }
 
     const onClickCode = () => {
@@ -94,13 +93,16 @@ const LiveRoom = () => {
     return (
         <div className="live-room" ref={ref}>
             <div className="left-space">
-                {showPeople && <div className="people-list">
+                <div className="people-list">
+                    <div className="people"><video autoPlay playsInline className="my-camera" ref={videoRef} />
+                        <div className="user-name">내 닉네임</div>
+                    </div>
                     {data.map((a, i) => {
                         return <div className="people"><img className="user-data" src={userImg} />
                             <div className="user-name">{a}</div>
                         </div>
                     })}
-                </div>}
+                </div>
 
                 <div className="screen">
                     <img className="screen-data" src={screenImg} />
@@ -138,22 +140,25 @@ const LiveRoom = () => {
 
             <div className="control-bar-wrapper">
                 <div className="control-bar">
-                    <div className="mike-button" ref={audioRef} autoPlay controls onClick={onClickMike}>
+                    <div className="mike-button" onClick={onClickMike}>
                         {isMikeOn ? <FontAwesomeIcon icon={faMicrophoneSlash} color={'white'} size={'2x'} /> :
                             <FontAwesomeIcon icon={faMicrophone} color={'white'} size={'2x'} />}
                     </div>
-                    <div className="video-button" onClick={onClickContentStream}>
+                    <div className="video-button" onClick={onClickCamera}>
                         {isCameraOn ? <FontAwesomeIcon icon={faVideoSlash} color={'white'} size={'2x'} /> :
                             <FontAwesomeIcon icon={faVideo} color={'white'} size={'2x'} />}
+                    </div>
+                    <div className="share-button" onClick={onClickContentStream}>
+                        <FontAwesomeIcon icon={faArrowUpFromBracket} color={'white'} size={'2x'} />
                     </div>
                     <div className="share-button" onClick={onClickCode}>
                         <FontAwesomeIcon icon={faCode} color={'white'} size={'2x'} />
                     </div>
                 </div>
                 <div className="control-bar" style={{ marginLeft: 180 }}>
-                    <div className="people-button" onClick={() => setShowPeople(!showPeople)}>
+                    {/* <div className="people-button" onClick={() => setShowPeople(!showPeople)}>
                         <FontAwesomeIcon icon={faUsers} color={'white'} size={'2x'} />
-                    </div>
+                    </div> */}
                     <div className="chat-button" onClick={() => setShowChat(!showChat)}>
                         <FontAwesomeIcon icon={faComment} color={'white'} size={'2x'} />
                     </div>
