c72930edfd7f23710cf837e4bd5955f5f94124e5
SCY <helenason@naver.com>
2023-10-09 16:21:34 +0900

    feat: 멘토링룸 준비 페이지 단체 채팅

--- backend/base.py
+++ backend/base.py
@@ -16,25 +16,43 @@ if __name__ == "__main__": # 해당 파일을 실행했을 경우
     # app.run(host="127.0.0.1", port="5000")
     socketio.run(app, host="127.0.0.1", port=5000)
 
-@socketio.on('connect')
+@socketio.on('connect', namespace='/study-ready')
 def test_connect():
-    print("========CONNECT========")
-
-@socketio.on('disconnect')
-def test_disconnect():
-    print('========DISCONNECT========')
-
-@socketio.on('enter')
-def enterRoom(data):
-    print("======ENTERROOM======")
-    roomId = data['roomId']
-    print(roomId)
-    join_room(roomId)
+    print("========스터디룸 준비 페이지 연결========")
     print(rooms())
 
-@socketio.on("sendTo")
+@socketio.on('connect', namespace='/mentoring-ready')
+def test_connect():
+    print("========멘토링룸 준비 페이지 연결========")
+    print(rooms())
+
+@socketio.on('disconnect', namespace='/study-ready')
+def test_disconnect():
+    print('========스터디룸 준비 페이지 연결 해제========')
+    print(rooms())
+
+@socketio.on('disconnect', namespace='/mentoring-ready')
+def test_disconnect():
+    print('========멘토링룸 준비 페이지 연결 해제========')
+    print(rooms())
+
+@socketio.on('enter', namespace='/study-ready')
+def enterRoom(data):
+    print("======스터디룸 준비 페이지 입장======")
+    roomId = data['roomId']
+    join_room('s'+str(roomId))
+    print(rooms())
+
+@socketio.on('enter', namespace='/mentoring-ready')
+def enterRoom(data):
+    print("======멘토링룸 준비 페이지 입장======")
+    roomId = data['roomId']
+    join_room('m'+str(roomId))
+    print(rooms())
+
+@socketio.on("sendTo", namespace='/study-ready')
 def sendMessage(data):
-    print("======SENDMSG======")
+    print("======스터디룸 메시지 전송======")
     roomId = data['roomId']
     message = data['message']
     sender = data['sender']
@@ -48,20 +66,11 @@ def sendMessage(data):
         'createdAt': now,
         'roomId': roomId
     })
-    emit('sendFrom', {'sender': sender, 'content': message, 'date': formatted_now}, to = roomId)
+    emit('sendFrom', {'sender': sender, 'content': message, 'date': formatted_now}, to = 's'+str(roomId))
 
-@socketio.on("leave")
-def leaveRoom(data):
-    print("=======LEAVEROOM=======")
-    roomId = data['roomId']
-    print(roomId)
-    leave_room(roomId)
-    print(rooms())
-
-@socketio.on("sendToRoom")
+@socketio.on("sendTo", namespace='/mentoring-ready')
 def sendMessage(data):
-    print("======SENDMSGROOM======")
-    
+    print("======멘토링룸 메시지 전송======")
     roomId = data['roomId']
     message = data['message']
     sender = data['sender']
@@ -69,25 +78,69 @@ def sendMessage(data):
     now = datetime.now()
     formatted_now = formatYMDHM(now)
 
-    emit('sendFromRoom', {'sender': sender, 'content': message, 'date': formatted_now}, to = roomId)
+    conn_mongodb().mentoringroom_chat.insert_one({
+        'sender': sender,
+        'content': message,
+        'createdAt': now,
+        'roomId': roomId
+    })
+    emit('sendFrom', {'sender': sender, 'content': message, 'date': formatted_now}, to = 'm'+str(roomId))
 
-@socketio.on('join_room', namespace='/live')
-def joinroom(roomId):
-    print("======조인룸======")
-    join_room(roomId)
-    emit('welcome', to=roomId)
+@socketio.on("leave", namespace='/study-ready')
+def leaveRoom(data):
+    print("=======스터디룸 준비 페이지 나감=======")
+    roomId = data['roomId']
+    leave_room('s'+str(roomId))
+    print(rooms())
 
-@socketio.on('offer', namespace='/live')
-def handle_offer(offer):
-    print("======offer=======")
-    emit('offer', offer)
+@socketio.on("leave", namespace='/mentoring-ready')
+def leaveRoom(data):
+    print("=======멘토링룸 준비 페이지 나감=======")
+    roomId = data['roomId']
+    leave_room('m'+str(roomId))
+    print(rooms())
 
-@socketio.on('answer', namespace='/live')
-def handle_answer(answer):
-    print("======answer=======")
-    emit('answer', answer)
+# @socketio.on('connect', namespace='/live')
+# def test_connect():
+#     print("========CONNECT live========")
+#     print(rooms())
 
-@socketio.on('ice', namespace='/live')
-def handle_ice(ice):
-    print("======ice=======")
-    emit('ice', ice)
+# @socketio.on('disconnect', namespace='/live')
+# def test_disconnect():
+#     print('========DISCONNECT live========')
+#     print(rooms())
+
+# @socketio.on("sendToRoom", namespace='/live')
+# def sendMessage(data):
+#     print("======SENDMSGROOM======")
+    
+#     roomId = data['roomId']
+#     message = data['message']
+#     sender = data['sender']
+
+#     now = datetime.now()
+#     formatted_now = formatYMDHM(now)
+
+#     emit('sendFromRoom', {'sender': sender, 'content': message, 'date': formatted_now}, to = roomId)
+
+# @socketio.on('join_room', namespace='/live')
+# def joinroom(roomId):
+#     print("======조인룸======")
+#     join_room(roomId)
+#     print(rooms())
+#     emit('welcome', to=roomId)
+
+# @socketio.on('offer', namespace='/live')
+# def handle_offer(offer):
+#     print("======offer=======")
+#     emit('offer', offer)
+
+# @socketio.on('answer', namespace='/live')
+# def handle_answer(answer):
+#     print("======answer=======")
+#     emit('answer', answer)
+
+# @socketio.on('ice', namespace='/live')
+# def handle_ice(ice):
+#     print("======ice=======")
+#     emit('ice', ice)
