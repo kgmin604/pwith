11e1f46a42de4e5cf7671136d3c79593f1d37a27
SCY <helenason@naver.com>
2023-11-03 18:48:36 +0900

    style: NodeJS DB 접근 코드 주석 처리

--- backend/model/db_mongo.js
+++ backend/model/db_mongo.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGO_SERVER =
-  "mongodb+srv://pwith:pwith1234@cluster0.ezfau5x.mongodb.net/";
+// const MONGO_SERVER =
+//   "mongodb+srv://pwith:pwith1234@cluster0.ezfau5x.mongodb.net/";
 
-const db = () => {
-  function connect() {
-    mongoose.connect(MONGO_SERVER, { dbName: "pwith_db" });
-  }
-  connect();
-  const conn = mongoose.connection;
-  conn.once("open", () => {
-    console.log("Connected to mongoDB");
-  });
-  conn.on("disconnected", connect);
-  conn.on("error", console.log);
-};
+// const db = () => {
+//   function connect() {
+//     mongoose.connect(MONGO_SERVER, { dbName: "pwith_db" });
+//   }
+//   connect();
+//   const conn = mongoose.connection;
+//   conn.once("open", () => {
+//     console.log("Connected to mongoDB");
+//   });
+//   conn.on("disconnected", connect);
+//   conn.on("error", console.log);
+// };
 
-export default db;
+// export default db;
